bd807934cebdab12f7e3bea9cf99f0fb006c3d46
rj0495 <rj0495@gmail.com>
2013-10-15 19:24:29 +0100

    Update TNT.php
    Added Fire and Shears to ignite TNT

--- src/material/block/misc/TNT.php
+++ src/material/block/misc/TNT.php
@@ -43,6 +43,41 @@ class TNTBlock extends SolidBlock{
 			ServerAPI::request()->api->entity->spawnToAll($e);
 			return true;
 		}
+		
+	if($item->getID() === FIRE){
+			if(($player->gamemode & 0x01) === 0){
+				$item->useOn($this);
+			}
+			$data = array(
+				"x" => $this->x + 0.5,
+				"y" => $this->y + 0.5,
+				"z" => $this->z + 0.5,
+				"power" => 4,
+				"fuse" => 20 * 4, //4 seconds
+			);
+			$this->level->setBlock($this, new AirBlock(), false, false, true);
+			$e = ServerAPI::request()->api->entity->add($this->level, ENTITY_OBJECT, OBJECT_PRIMEDTNT, $data);
+			ServerAPI::request()->api->entity->spawnToAll($e);
+			return true;
+			}
+			
+			if($item->getID() === SHEARS){
+			if(($player->gamemode & 0x01) === 0){
+				$item->useOn($this);
+			}
+			$data = array(
+				"x" => $this->x + 0.5,
+				"y" => $this->y + 0.5,
+				"z" => $this->z + 0.5,
+				"power" => 4,
+				"fuse" => 20 * 4, //4 seconds
+			);
+			$this->level->setBlock($this, new AirBlock(), false, false, true);
+			$e = ServerAPI::request()->api->entity->add($this->level, ENTITY_OBJECT, OBJECT_PRIMEDTNT, $data);
+			ServerAPI::request()->api->entity->spawnToAll($e);
+			return true;
+			}
+			
 		return false;
 	}
 }
