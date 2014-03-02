024d053e4781c3d3c9407378e0d622e1aec8b3e6
jcliving <jcliving888@yahoo.com>
2014-03-01 21:30:37 -0600

    Update HardenedClay.php
    No coloring for hardened clay.  Just for stained hardened clay.

--- src/material/block/solid/HardenedClay.php
+++ src/material/block/solid/HardenedClay.php
@@ -19,28 +19,9 @@
  *
 */
 
-class HardenedBlock extends SolidBlock{
+class HardenedClayBlock extends SolidBlock{
 	public function __construct($meta = 0){
 		parent::__construct(HARDENED_CLAY, $meta, "hardened_clay");
-		$names = array(
-			0 => "White Stained Clay",
-			1 => "Orange Stained Clay",
-			2 => "Magenta Stained Clay",
-			3 => "Light Blue Stained Clay",
-			4 => "Yellow Stained Clay",
-			5 => "Lime Stained Clay",
-			6 => "Pink Stained Clay",
-			7 => "Gray Stained Clay",
-			8 => "Light Gray Stained Clay",
-			9 => "Cyan Stained Clay",
-			10 => "Purple Stained Clay",
-			11 => "Blue Stained Clay",
-			12 => "Brown Stained Clay",
-			13 => "Green Stained Clay",
-			14 => "Red Stained Clay",
-			15 => "Black Stained Clay",
-		);
-		$this->name = $names[$this->meta];
 		$this->hardness = 30;
 	}
 	
