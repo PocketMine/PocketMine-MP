b8fcb5b3c75dd07a1e853c18181143a78a976246
PEMapModder <pemapmodder1970@gmail.com>
2014-05-02 18:36:55 +0800

    Used preg
    Used regular expression for that

--- src/pocketmine/utils/Config.php
+++ src/pocketmine/utils/Config.php
@@ -93,7 +93,13 @@ class Config{
 	 */
 	public static function fixYAMLIndexes($str){
 		$result = preg_replace("#^([ ]*)([a-zA-Z_]{1}[^\:]*)\:#m", "$1\"$2\":", $str);
-		$result = str_replace("\t", "    ", $result);
+		while(preg_match_all("#".PHP_EOL."([\t]{1,})#", $result, $matches) > 0){
+			$match = $matches[0][0];
+			$tmp = strstr($result, $match, true);
+			$tmp2 = str_replace("\t", "    ", $match);
+			$tmp3 = substr($result, strlen($tmp) + strlen($match));
+			$result = $tmp . $tmp2 . $tmp3;
+		}
 		return $result;
 	}
 
