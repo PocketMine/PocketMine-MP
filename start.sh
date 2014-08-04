8d8ad7c9d279178db407d5afb91c645a6c4eac62
williamtdr <william@cboos.com>
2014-08-04 10:09:09 -0500

    Auto-restart utils

--- start.sh
+++ start.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 DIR="$(cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 cd "$DIR"
-if [ -f ./bin/php5/bin/php ]; then
-	exec ./bin/php5/bin/php -d enable_dl=On PocketMine-MP.php $@
-else
-	echo "[WARNING] You are not using the standalone PocketMine-MP PHP binary."
-	exec php -d enable_dl=On PocketMine-MP.php $@
-fi
+while [ true ]
+do
+./bin/php5/bin/php -d enable_dl=On PocketMine-MP.php $@
+echo "Restarting the server..."
+done
