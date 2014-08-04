1ec29dd9df89d0977e55e610fae904a4f9d8f2dc
williamtdr <william@cboos.com>
2014-08-04 10:19:05 -0500

    Main file renamed to fit with separate naming struct

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@ DIR="$(cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 cd "$DIR"
 while [ true ]
 do
-./bin/php5/bin/php -d enable_dl=On PocketMine-MP.php $@
+./bin/php5/bin/php -d enable_dl=On Steadfast.php $@
 echo "Restarting the server..."
 done
