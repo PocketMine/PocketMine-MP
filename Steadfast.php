fc9f6cd1c943cea86fdd456c8585de59b43d6601
williamtdr <william@cboos.com>
2014-08-05 08:55:26 -0500

    Move custom options file.

--- Steadfast.php
+++ Steadfast.php
@@ -25,8 +25,6 @@ require_once(dirname(__FILE__)."/src/config.php");
 
 require_once(FILE_PATH."/src/functions.php");
 require_once(FILE_PATH."/src/dependencies.php");
-
-require_once("custom_options.php");
 /***REM_END***/
 
 $server = new ServerAPI();
