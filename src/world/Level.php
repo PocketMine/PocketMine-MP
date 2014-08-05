3f0bf124334bc389d8ccdc89b3da83512cc7adb6
williamtdr <william@cboos.com>
2014-08-04 19:25:38 -0500

    Remove any echoing.

--- src/world/Level.php
+++ src/world/Level.php
@@ -447,7 +447,6 @@ class Level{
         for($send_x = 0; $send_x <= $max_x; $send_x++) {
             for($send_z = 0; $send_z <= $max_z; $send_z++) {
                 for($send_y = 0; $send_y <= 127; $send_y++) {
-                    echo "{".$send_x.",".$send_y.",".$send_z."}\r";
                     //$orderedIds .= "\x" . $this->level->getBlock($send_x,$send_y,$send_z)->getID();
                     //$orderedData .= "\x" . $this->level->getBlock($send_x,$send_y,$send_z)->getMetadata();
                 }
