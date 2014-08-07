4f0109cd55ac5ae0590d37434ddae18880cbe824
KnownUnown <andrew.tianyi.pan@gmail.com>
2014-08-07 13:57:23 -0500

    Test commit.

--- src/world/Level.php
+++ src/world/Level.php
@@ -464,7 +464,7 @@ class Level{
 					$miniChunk = $this->level->getMiniChunk($X, $Z, $y);
 					$miniChunkIndex = ($aX << 5) + ($aZ << 9);
 					$orderedIds .= substr($miniChunk, $miniChunkIndex, 16);
-					$miniChunkIndex = 4096 + ((($aX << 5) + ($aZ << 9)) >> 1);
+					$miniChunkIndex = 16 + ((($aX << 5) + ($aZ << 9)));
 					$orderedData .= substr($miniChunk, $miniChunkIndex, 8);
 					//$miniChunkIndex += 2048;
 					//$orderedLight .= substr($miniChunk, $miniChunkIndex, 8);
