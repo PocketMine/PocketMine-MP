0d533c92dd37088bb86e8f9bec2251ad504f41f2
KnownUnown <andrew.tianyi.pan@gmail.com>
2014-08-08 10:52:45 -0500

    shog-fix

--- src/world/Level.php
+++ src/world/Level.php
@@ -465,7 +465,7 @@ class Level{
 			$miniChunks[$y] = $this->level->getMiniChunk($X, $Z, $y);
 		}
 		
-		for($j = 0; $j < 256; ++$j){
+		for($j = 255; $j >= 0; --$j){
 			foreach($miniChunks as $chunk){
 				$orderedIds .= substr($chunk, $bIndex = ($j << 5), 16);
 				$orderedData .= substr($chunk, $bIndex + 16, 8);
