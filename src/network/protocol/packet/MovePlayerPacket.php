8795ecfd4036161ac87dbe88237fbf6e3c9f16dc
williamtdr <william@cboos.com>
2014-08-12 10:21:09 -0500

    Revert "Adjust for packet changes in 0.9"
    This reverts commit c0989a57f65f0d40d621fcb61be7106a4366dd04.

--- src/network/protocol/packet/MovePlayerPacket.php
+++ src/network/protocol/packet/MovePlayerPacket.php
@@ -35,7 +35,7 @@ class MovePlayerPacket extends RakNetDataPacket{
 	public function decode(){
 		$this->eid = $this->getInt();
 		$this->x = $this->getFloat();
-		$this->y = $this->getFloat() - 1.8;
+		$this->y = $this->getFloat();
 		$this->z = $this->getFloat();
 		$this->yaw = $this->getFloat();
 		$this->pitch = $this->getFloat();
@@ -46,7 +46,7 @@ class MovePlayerPacket extends RakNetDataPacket{
 		$this->reset();
 		$this->putInt($this->eid);
 		$this->putFloat($this->x);
-		$this->putFloat($this->y + 1.8);
+		$this->putFloat($this->y);
 		$this->putFloat($this->z);
 		$this->putFloat($this->yaw);
 		$this->putFloat($this->pitch);
