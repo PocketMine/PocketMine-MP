8bf29bd5bbc264eb8045572c9461b786e7de585b
KnownUnown <andrew.tianyi.pan@gmail.com>
2014-08-12 12:43:37 -0500

    Fixed ContainerOpenPacket.

--- src/network/protocol/packet/ContainerOpenPacket.php
+++ src/network/protocol/packet/ContainerOpenPacket.php
@@ -39,7 +39,7 @@ class ContainerOpenPacket extends RakNetDataPacket{
 		$this->reset();
 		$this->putByte($this->windowid);
 		$this->putByte($this->type);
-		$this->putByte($this->slots);
+		$this->putShort($this->slots);
 		$this->putInt($this->x);
 		$this->putInt($this->y);
 		$this->putInt($this->z);
