980b823498878ac7a9e38a2359d1b63612ae600f
Shoghi Cervantes <shoghicp@gmail.com>
2014-08-13 06:57:33 -0400

    Added phar creation script

--- start_once.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-DIR="$(cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
-cd "$DIR"
-if [ -f ./bin/php5/bin/php ]; then
-	exec ./bin/php5/bin/php -d enable_dl=On Steadfast.php $@
-else
-	echo "[WARNING] You are not using the standalone PocketMine-MP PHP binary."
-	exec php -d enable_dl=On Steadfast.php $@
-fi
