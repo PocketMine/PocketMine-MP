fde49b1c7bb0e58da1e3b660b4c03604b65c403c
williamtdr <william@cboos.com>
2014-08-17 18:07:31 -0500

    Revert "Possible fix for overloaded method notice."
    This reverts commit c95126b3915ec6312fc9c92d13e06857b1e70f90.

--- src/Player.php
+++ src/Player.php
@@ -90,13 +90,6 @@ class Player{
 		}
 		return null;
 	}
-	
-	public function &__get($name){
-		if(isset($this->{$name})){
-			return ($this->{$name});
-		}
-		return null;
-	}
 
 	/**
 	 * @param integer $clientID
