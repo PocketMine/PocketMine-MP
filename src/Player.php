7de0bbfb8e3b74e3f5475426dbab4d412e4ee3dc
williamtdr <william@cboos.com>
2014-08-31 11:51:49 -0500

    Force sprint-hacks off for non-opped players

--- src/Player.php
+++ src/Player.php
@@ -1475,7 +1475,7 @@ class Player{
 						}
 					}
 					$speed = $this->entity->getSpeedMeasure();
-					if($this->blocked === true or ($this->server->api->getProperty("allow-flight") !== true and (($speed > 9 and ($this->gamemode & 0x01) === 0x00) or $speed > 20 or $this->entity->distance($newPos) > 7)) or $this->server->api->handle("player.move", $this->entity) === false){
+					if($this->blocked === true or ((($speed > 9 and ($this->gamemode & 0x01) === 0x00) or $speed > 20 or $this->entity->distance($newPos) > 7)) or $this->server->api->handle("player.move", $this->entity) === false){
 						if($this->lastCorrect instanceof Vector3){
 							$this->teleport($this->lastCorrect, $this->entity->yaw, $this->entity->pitch, false);
 						}
