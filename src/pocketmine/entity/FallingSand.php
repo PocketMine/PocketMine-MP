c016f9c1ce6ff30560fc892cfd1be6a32315e8c9
MinecrafterJPN <minecrafter.mn@gmail.com>
2015-03-15 23:18:20 +0900

    Use (int) instead of floor()

--- src/pocketmine/entity/FallingSand.php
+++ src/pocketmine/entity/FallingSand.php
@@ -108,7 +108,7 @@ class FallingSand extends Entity{
 			$this->motionY *= 1 - $this->drag;
 			$this->motionZ *= $friction;
 
-			$pos = new Vector3(floor($this->x), round($this->y), floor($this->z));
+			$pos = new Vector3((int)$this->x, round($this->y), (int)$this->z);
 
 			if($this->onGround){
 				$this->kill();
