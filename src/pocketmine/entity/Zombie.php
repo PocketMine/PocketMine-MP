71e62627523b33dc79c7075264835b2169e05cd2
yungtechboy1 <yungtechboy1@gmail.com>
2015-04-14 12:14:44 -0500

    Adds Zombie AI

--- src/pocketmine/entity/Zombie.php
+++ src/pocketmine/entity/Zombie.php
@@ -1,5 +1,7 @@
 <?php
 
+/* <?php
+
 /*
  *
  *  ____            _        _   __  __ _                  __  __ ____  
@@ -22,24 +24,34 @@
 namespace pocketmine\entity;
 
 
-use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\item\Item as ItemItem;
 use pocketmine\network\protocol\AddEntityPacket;
 use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\math\Vector3;
+use pocketmine\nbt\tag\Int;
+use pocketmine\math\AxisAlignedBB;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\entity\EntityRegainHealthEvent;
+use pocketmine\entity\Monster;
 
 class Zombie extends Monster{
+    
 	const NETWORK_ID = 32;
-
 	public $width = 0.6;
 	public $length = 0.6;
 	public $height = 1.8;
+        public $limit = 0;
+        private $randomtick;
+        private $jumptick;
 
-	public function getName(){
+        public function getName(){
 		return "Zombie";
 	}
 
 	public function spawnTo(Player $player){
-		$pk = new AddEntityPacket();
+                $pk = new AddEntityPacket();
 		$pk->eid = $this->getId();
 		$pk->type = Zombie::NETWORK_ID;
 		$pk->x = $this->x;
@@ -52,25 +64,178 @@ class Zombie extends Monster{
 		$pk->pitch = $this->pitch;
 		$pk->metadata = $this->dataProperties;
 		$player->dataPacket($pk);
-
 		parent::spawnTo($player);
 	}
 
+	public function getData(){ //TODO
+		$flags = 0;
+		$flags |= $this->fireTicks > 0 ? 1 : 0;
+		//$flags |= ($this->crouched === true ? 0b10:0) << 1;
+		//$flags |= ($this->inAction === true ? 0b10000:0);
+		$d = [
+			0 => ["type" => 0, "value" => $flags],
+			1 => ["type" => 1, "value" => $this->airTicks],
+			16 => ["type" => 0, "value" => 0],
+			17 => ["type" => 6, "value" => [0, 0, 0]],
+		];
+
+		return $d;
+	}
+
+        public function getBoundingBox(){
+		$this->boundingBox = new AxisAlignedBB(
+			$x = $this->x - $this->width / 2,
+			$y = $this->y - $this->stepHeight,
+			$z = $this->z - $this->length / 2,
+			$x + $this->width,
+			$y + $this->height,
+			$z + $this->length
+		);
+		return $this->boundingBox;
+	}
+        
+        public function updateDirection($yaw , $pitch){   
+            foreach($this->hasSpawned as $player){
+                $player->addEntityMovement($this->getId(), $this->x, $this->y, $this->z, $yaw, $pitch);
+            }
+        }
+	
+	public function updateMove(Vector3 $vec, Player $p = null){
+		$this->x = $vec->x;
+		$this->y = $vec->y;
+		$this->z = $vec->z;
+                foreach($this->hasSpawned as $player){
+			$p !== null ?
+                                $player->addEntityMovement($this->getId(), $this->x, $this->y, $this->z, $this->generateYaw($p), $this->generatePitch($p))
+                                : $player->addEntityMovement($this->getId(), $this->x, $this->y, $this->z, mt_rand(0, 360), mt_rand(0, 90));
+		}
+	}
+	public function generateYaw(Player $target){
+            $x = $target->x - $this->x;
+            $y = $target->y - $this->y;
+            $z = $target->z - $this->z;
+            $atn = atan2($z, $x);
+            return rad2deg($atn - M_PI_2);
+	}
+        
+        public function generatePitch(Player $target) {
+            $x = $target->x - $this->x;
+            $y = $target->y - $this->y;
+            $z = $target->z - $this->z;
+            return rad2deg(-atan2($y, sqrt($x ** 2 + $z ** 2)));
+        }
+        
+        public function isDay() {
+           $time = $this->getLevel()->getTime();
+           if((12000 > $time) && ( $time > 23001)){
+               $this->setOnFire(1);
+           }
+        }
+        
+         public function onUpdate($currentTick){
+             $this->entityBaseTick();
+             $this->isDay();
+            foreach ($this->hasSpawned as $p){
+                    $dis = sqrt(pow($dZ = $p->getZ() - $this->z, 2) + pow($dX = $p->getX() - $this->x, 2));
+                    if($dis <= 10 && $p->isSurvival() && $p->spawned && !$p->dead){
+                            $this->motionX = 0;
+                            $this->motionZ = 0;
+                            $this->motionY = 0;
+                            $target = $p;
+                            $x = $target->x - $this->x;
+                            $y = $target->y - $this->y;
+                            $z = $target->z - $this->z;
+                            $atn = atan2($z, $x);
+
+                            $this->motionX = /*$this->x + */(cos($atn) * 0.1);
+                            $this->motionZ = /*$this->z + */(sin($atn) * 0.1);
+                            if($dis < 1.3 && (max($p->getY(),$this->y) - min($p->getY(),$this->y)) <= 2 ){
+                                $p->knockBack($this, 1, ($this->motionX) /5,($this->motionZ) /5 ,.2); 
+                                $p->attack(1, new EntityDamageEvent($this, 1, 1));
+                            }
+                           
+                            $bb = clone $this->getBoundingBox();
+                            $onGround = count($this->level->getCollisionBlocks($bb->offset(0, -.1, 0))) > 0;
+                            if(!$onGround){$this->motionY = -.1;}else{$this->motionY = 0;};
+                            $isJump = $this->isJumpable();
+                            $noRoof = count($this->level->getCollisionBlocks($bb->grow(0,0,0)->offset($this->motionX, 2, $this->motionZ))) == 0;
+                            if($isJump && $noRoof && $this->jumptick < 0){$this->jumptick = 35;$this->motionY = 1;$this->motionX /= 10;$this->motionZ /= 10;}
+                            if($this->isMoveable()){
+                                $this->updateMove(new Vector3($this->x + $this->motionX, $this->y + $this->motionY,$this->z + $this->motionZ), $p);
+                            }else{
+                                $this->updateDirection($this->generateYaw($p), $this->generatePitch($p));
+                                //$this->MoveAround();
+                            }
+                }else{
+                    if($this->randomtick <= 0){
+                        $this->randomtick = 20 * mt_rand(1, 30);//Random 30 Secs
+                        $this->motionZ = mt_rand(-1, 1);
+                        $this->motionX = mt_rand(-1, 1);
+                        echo $this->x + $this->motionX."_".  $this->y."_".($this->z + $this->motionZ)."\n";
+                        $this->updateMove(new Vector3($this->x + $this->motionX,  $this->y, ($this->z + $this->motionZ)), null);
+                }
+            }
+            $this->randomtick--;
+            $this->jumptick--;
+        }
+        }
+        
+        public function isMoveable() {
+            $bb = clone $this->getBoundingBox();
+            if ($this->motionX > 0 ) $ax = 1;
+            if ($this->motionX < 0 ) $ax = -1;
+            if ($this->motionX == 0 ) $ax = 0;
+            if ($this->motionZ > 0 ) $az = 1;
+            if ($this->motionZ < 0 ) $az = -1;
+            if ($this->motionZ == 0 ) $az = 0;
+                
+                if(count($this->level->getCollisionBlocks($bb->grow(0,0,0)->offset($ax, 0, $az))) > 0){
+                    if(count($this->level->getCollisionBlocks($bb->grow(0,0,0)->offset($this->motionX, 0, $this->motionZ))) < 0){
+                        return true;
+                     }   else{
+                     return false;
+                     }
+                }
+            return true;
+        }
+        
+        public function isJumpable() {
+            $bb = clone $this->getBoundingBox();
+            $this->motionX > 0  ? $ax = 1 : $ax = -1;
+            $this->motionZ > 0  ? $az = 1 : $az = -1;
+            
+            if(count($this->level->getCollisionBlocks($bb->grow(0,0,0)->offset($ax, 0, $az))) > 0){
+                if(count($this->level->getCollisionBlocks($bb->grow(0,0,0)->offset($ax, 2, $az))) > 0){
+                    return false;
+                }
+                if(count($this->level->getCollisionBlocks($bb->grow(0,0,0)->offset(0, 2, 0))) > 0){
+                    return false;
+                }
+                if(count($this->level->getCollisionBlocks($bb->grow(0,0,0)->offset($ax, .9, $az))) <= 0){
+                    return true;
+                }
+                if(count($this->level->getCollisionBlocks($bb->grow(0,0,0)->offset(0, .9, 0))) <= 0){
+                    return true;
+                }
+            }
+            return false;
+        }
+        
 	public function getDrops(){
 		$drops = [
 			ItemItem::get(ItemItem::FEATHER, 0, 1)
 		];
 		if($this->lastDamageCause instanceof EntityDamageByEntityEvent and $this->lastDamageCause->getEntity() instanceof Player){
-			if(mt_rand(0, 199) < 5){
+			if(mt_rand(0, 199) < 50){//1 in 4 Chance
 				switch(mt_rand(0, 2)){
 					case 0:
-						$drops[] = ItemItem::get(ItemItem::IRON_INGOT, 0, 1);
+						$drops[] = ItemItem::get(ItemItem::IRON_INGOT, 0, mt_rand(0, 3));
 						break;
 					case 1:
-						$drops[] = ItemItem::get(ItemItem::CARROT, 0, 1);
+						$drops[] = ItemItem::get(ItemItem::CARROT, 0, mt_rand(0, 3));
 						break;
 					case 2:
-						$drops[] = ItemItem::get(ItemItem::POTATO, 0, 1);
+						$drops[] = ItemItem::get(ItemItem::POTATO, 0, mt_rand(0, 3));
 						break;
 				}
 			}
@@ -78,4 +243,27 @@ class Zombie extends Monster{
 
 		return $drops;
 	}
+        
+        public function heal($amount, EntityRegainHealthEvent $source){
+            //$this->setHealth($amount);
+            parent::heal($amount, $source);
+            /*$pk = new SetHealthPacket();
+            $pk->health = $this->getHealth();
+            $this->dataPacket($pk);*/
+	}
+        
+        public function kill() {
+            if($this->getLastDamageCause()){
+            $d = $this->getLastDamageCause()->getEntity();
+            if ($d instanceof Player){
+                $this->server->broadcastMessage($d->getName()." Has Just Killed a Zombie!");
+            }
+            }
+            parent::kill();
+        }
+        
+        public function attack($damage, EntityDamageEvent $source){
+            parent::attack($damage, $source);
+        }
 }
+
