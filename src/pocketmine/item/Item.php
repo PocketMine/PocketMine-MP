81ff69a0b6b54bc107366ef1895d3e69f0e8f239
PeratX <wangshigroup@gmailc.com>
2015-10-05 12:01:55 +0800

    Fixed Item->deepEquals always returns true

--- src/pocketmine/item/Item.php
+++ src/pocketmine/item/Item.php
@@ -1383,7 +1383,7 @@ class Item{
 	}
 
 	public final function deepEquals(Item $item, $checkDamage = true, $checkCompound = true){
-		if($item->equals($item, $checkDamage, $checkCompound)){
+		if($this->equals($item, $checkDamage, $checkCompound)){
 			return true;
 		}elseif($item->hasCompoundTag() or $this->hasCompoundTag()){
 			return NBT::matchTree($this->getNamedTag(), $item->getNamedTag());
