7d2b0418d83badb6f87e459feb13d54c8bf1ba1d
ZenaGamingsky <ZenaGamingsky@gmail.com>
2015-10-24 10:52:23 +0100

    Update Item.php

--- src/pocketmine/item/Item.php
+++ src/pocketmine/item/Item.php
@@ -31,6 +31,7 @@ use pocketmine\entity\Entity;
 use pocketmine\entity\Squid;
 use pocketmine\entity\Villager;
 use pocketmine\entity\Zombie;
+use pocketmine\entity\Wolf;
 use pocketmine\inventory\Fuel;
 use pocketmine\item\enchantment\Enchantment;
 use pocketmine\level\Level;
@@ -838,7 +839,7 @@ class Item{
 		//self::addCreativeItem(Item::get(Item::SPAWN_EGG, 11)); //Cow
 		//self::addCreativeItem(Item::get(Item::SPAWN_EGG, 12)); //Pig
 		//self::addCreativeItem(Item::get(Item::SPAWN_EGG, 13)); //Sheep
-		//TODO: Wolf
+		self::addCreativeItem(Item::get(Item::SPAWN_EGG, Wolf::NETWORK_ID));
 		//TODO: Mooshroom
 		//TODO: Creeper
 		//TODO: Enderman
