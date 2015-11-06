1ddae79b0a6eadd59f6db3c249d9e5dbba57c0da
Diego <skygridrex@gmail.com>
2015-11-06 17:33:42 -0500

    Update NetherWart.php

--- src/pocketmine/item/NetherWart.php
+++ src/pocketmine/item/NetherWart.php
@@ -21,6 +21,8 @@
 
 namespace pocketmine\item;
 
+use pocketmine\block\block
+
 class NetherWart extends Item{
 	public function __construct($meta = 0, $count = 1){
 		parent::__construct(self::NETHER_WART, $meta, $count, "Nether Wart");
