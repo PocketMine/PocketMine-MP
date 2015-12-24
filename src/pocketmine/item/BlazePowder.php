d5c4c72ee3b83b92d62921fe6b5970a3e0cc1541
XFuryMCPE <xfurymcpe@gmail.com>
2015-12-24 16:39:11 -0500

    Update BlazePowder.php

--- src/pocketmine/item/BlazePowder.php
+++ src/pocketmine/item/BlazePowder.php
@@ -18,7 +18,7 @@
  *
 */
 namespace pocketmine\item;
-class Camera extends Item{
+class BlazePowder extends Item{
 	public function __construct($meta = 0, $count = 1){
 		parent::__construct(self::BLAZE_POWDER, $meta, $count, "Blaze Powder");
 	}
