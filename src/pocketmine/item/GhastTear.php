6bc57b82cac8e5ae64ab2a2308ac9956281953b4
XFuryMCPE <xfurymcpe@gmail.com>
2015-12-24 16:41:54 -0500

    Update GhastTear.php

--- src/pocketmine/item/GhastTear.php
+++ src/pocketmine/item/GhastTear.php
@@ -18,7 +18,7 @@
  *
 */
 namespace pocketmine\item;
-class Camera extends Item{
+class GhastTear extends Item{
 	public function __construct($meta = 0, $count = 1){
 		parent::__construct(self::GHAST_TEAR, $meta, $count, "Ghast Tear");
 	}
