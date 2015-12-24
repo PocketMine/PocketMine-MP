de0e9c985b4ac73583f54dd4de34e366e19d836a
PEMapModder <pemapmodder1970@gmail.com>
2015-12-24 12:08:22 +0800

    Change Item constructor visibility to protected
    External callers should use `Item::get()` instead of `new Item`

--- src/pocketmine/item/Item.php
+++ src/pocketmine/item/Item.php
@@ -1129,7 +1129,7 @@ class Item{
 		}
 	}
 
-	public function __construct($id, $meta = 0, $count = 1, $name = "Unknown"){
+	protected function __construct($id, $meta = 0, $count = 1, $name = "Unknown"){
 		$this->id = $id & 0xffff;
 		$this->meta = $meta !== null ? $meta & 0xffff : null;
 		$this->count = (int) $count;
