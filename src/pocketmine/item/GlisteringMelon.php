292e4713ed50dce3d85d37084107a5f0cb8b4894
XFuryMCPE <xfurymcpe@gmail.com>
2015-12-24 16:38:43 -0500

    Update GlisteringMelon.php

--- src/pocketmine/item/GlisteringMelon.php
+++ src/pocketmine/item/GlisteringMelon.php
@@ -18,7 +18,7 @@
  *
 */
 namespace pocketmine\item;
-class Camera extends Item{
+class GlisteringMelon extends Item{
 	public function __construct($meta = 0, $count = 1){
 		parent::__construct(self::GLISTERING_MELON, $meta, $count, "Glistering Melon");
 	}
