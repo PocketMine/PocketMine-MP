b010bb40a18c6de3eee82a23bad8941c9285e4a5
PocketServers <Zena196970@gmail.com>
2016-01-04 19:05:34 +0000

    Create RedstoneBlock.php
    Redstone Block

---
+++ src/pocketmine/block/RedstoneBlock.php
@@ -0,0 +1,88 @@
+<?php
+
+/*
+ *
+ *  ____            _        _   __  __ _                  __  __ ____  
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \ 
+ * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
+ * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/ 
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_| 
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author PocketMine Team
+ * @link http://www.pocketmine.net/
+ * 
+ *
+*/
+
+namespace pocketmine\block;
+
+use pocketmine\item\Item;
+use pocketmine\item\Tool;
+use pocketmine\level\Level;
+use pocketmine\Player;
+
+class RedstoneBlock extends Solid implements Redstone{
+
+	protected $id = self::REDSTONE_BLOCK;
+
+	public function __construct(){
+
+	}
+
+	public function getHardness(){
+		return 5;
+	}
+	
+	public function getPower(){
+		return 15;
+	}
+	
+	public function BroadcastRedstoneUpdate($type,$power){
+		for($side = 0; $side <= 5; $side++){
+			$around=$this->getSide($side);
+			$this->getLevel()->setRedstoneUpdate($around,Block::REDSTONEDELAY,$type,$power);
+		}
+	}
+	
+	public function onRedstoneUpdate($type,$power){
+		if($type == Level::REDSTONE_UPDATE_PLACE or $type == Level::REDSTONE_UPDATE_LOSTPOWER){
+			$this->BroadcastRedstoneUpdate(Level::REDSTONE_UPDATE_NORMAL,$this->getPower());
+			return;
+		}
+		return;
+	}
+	
+	public function place(Item $item, Block $block, Block $target, $face, $fx, $fy, $fz, Player $player = null){
+		$this->BroadcastRedstoneUpdate(Level::REDSTONE_UPDATE_PLACE,$this->getPower(),$this);
+		return $this->getLevel()->setBlock($this, $this, true, true);
+	}
+	
+	public function getToolType(){
+		return Tool::TYPE_PICKAXE;
+	}
+
+	public function getName(){
+		return "Redstone Block";
+	}
+	
+	public function onBreak(Item $item){
+		$oBreturn = $this->getLevel()->setBlock($this, new Air(), true, true);
+		$this->BroadcastRedstoneUpdate(Level::REDSTONE_UPDATE_BREAK,$this->getPower());
+		return $oBreturn;
+	}
+	
+	public function getDrops(Item $item){
+		if($item->isPickaxe() >= Tool::TIER_WOODEN){
+			return [
+				[Item::REDSTONE_BLOCK, 0, 1],
+			];
+		}else{
+			return [];
+		}
+	}
+}
