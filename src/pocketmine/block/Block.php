863ae7d6be7e5fc2c4dce5f0dbb4047a1ef29dbd
Samueljh1 <samueljh2001@gmail.com>
2016-01-16 14:53:36 +0000

    Update Block.php
    Adds event caller to BlockActivateEvent

--- src/pocketmine/block/Block.php
+++ src/pocketmine/block/Block.php
@@ -38,6 +38,8 @@ use pocketmine\metadata\Metadatable;
 use pocketmine\metadata\MetadataValue;
 use pocketmine\Player;
 use pocketmine\plugin\Plugin;
+use pocketmine\Server;
+use pocketmine\event\block\BlockActivateEvent;
 
 
 class Block extends Position implements Metadatable{
@@ -603,6 +605,7 @@ class Block extends Position implements Metadatable{
 	 * @return bool
 	 */
 	public function onActivate(Item $item, Player $player = null){
+		Server::getInstance()->getPluginManager()->callEvent($ev = new BlockActivateEvent($this, $item, $player));
 		return false;
 	}
 
