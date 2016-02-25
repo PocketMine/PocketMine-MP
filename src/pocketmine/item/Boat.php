054288e790a18bdb643d7fca7f79fcf2e2e8165e
ondraczsk <ondrakraftak@gmail.com>
2016-02-25 21:13:10 +0100

    Boat can be placeable
    Update boat

--- src/pocketmine/item/Boat.php
+++ src/pocketmine/item/Boat.php
@@ -20,9 +20,67 @@
 */
 
 namespace pocketmine\item;
-
-class Boat extends Item {
-    public function __construct($meta = 0, $count =1){
-        parent::__construct(self::BOAT, $meta, $count, "Boat");
-    }
+use pocketmine\level\Level;
+use pocketmine\block\Block;
+use pocketmine\Player;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\nbt\tag\ListTag;
+use pocketmine\nbt\tag\DoubleTag;
+use pocketmine\nbt\tag\FloatTag;
+use pocketmine\entity\Boat as BoatEntity;
+class Boat extends Item{
+	public function __construct($meta = 0, $count = 1){
+		parent::__construct(self::BOAT, $meta, $count, "Oak Boat");
+		if($this->meta === 1){
+			$this->name = "Spruce Boat";
+		}elseif($this->meta === 2){
+			$this->name = "Birch Boat";
+		}elseif($this->meta === 3){
+			$this->name = "Jungle Boat";
+		}elseif($this->meta === 4){
+			$this->name = "Acacia Boat";
+		}elseif($this->meta === 5){
+			$this->name = "Dark Oak Boat";
+		}
+	}
+	
+	public function getMaxStackSize() : int{
+		return 1;
+	}
+	
+	public function canBeActivated() : bool{
+		return true;
+	}
+	public function onActivate(Level $level, Player $player, Block $block, Block $target, $face, $fx, $fy, $fz){
+		$boatPos = $block->getSide($face);
+		$boat = new BoatEntity($player->getLevel()->getChunk($boatPos->getX() >> 4, $boatPos->getZ() >> 4), new CompoundTag("", [
+			"Pos" => new ListTag("Pos", [
+				new DoubleTag("", $boatPos->getX()),
+				new DoubleTag("", $boatPos->getY()),
+				new DoubleTag("", $boatPos->getZ())
+			]),
+			"Motion" => new ListTag("Motion", [
+				new DoubleTag("", 0),
+				new DoubleTag("", 0),
+				new DoubleTag("", 0)
+			]),
+			"Rotation" => new ListTag("Rotation", [
+				new FloatTag("", 0),
+				new FloatTag("", 0)
+			]),
+		]));
+		$boat->spawnToAll();
+		if($player->isSurvival()) {
+			$item = $player->getInventory()->getItemInHand();
+			$count = $item->getCount();
+			if(--$count <= 0){
+				$player->getInventory()->setItemInHand(Item::get(Item::AIR));
+				return;
+			}
+			$item->setCount($count);
+			$player->getInventory()->setItemInHand($item);
+		}
+		
+		return true;
+	}
 }
