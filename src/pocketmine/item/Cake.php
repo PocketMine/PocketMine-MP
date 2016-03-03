55b5fc7feee5ddfd48d4bfb31f6d6959fa78bb85
legoboy0215 <legoboy0215@users.noreply.github.com>
2016-03-03 08:51:39 +0800

    Stupid mistake...

--- src/pocketmine/item/Cake.php
+++ src/pocketmine/item/Cake.php
@@ -23,7 +23,7 @@ namespace pocketmine\item;
 
 use pocketmine\block\Block;
 
-class Cake extends Food{
+class Cake extends Item{
 	public function __construct($meta = 0, $count = 1){
 		$this->block = Block::get(Item::CAKE_BLOCK);
 		parent::__construct(self::CAKE, 0, $count, "Cake");
