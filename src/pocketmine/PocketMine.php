9cc0161bb77fe128d57370e1c944cc3730d15c53
Tim (robske_110) <robske110@users.noreply.github.com>
2016-04-23 20:37:55 +0200

    Maybe this is better for the networking?

--- src/pocketmine/PocketMine.php
+++ src/pocketmine/PocketMine.php
@@ -76,7 +76,7 @@ namespace pocketmine {
 	const API_VERSION = "2.0.0";
 	const CODENAME = "[REDACTED]";
 	const MINECRAFT_VERSION = "v0.14.1 alpha";
-	const MINECRAFT_VERSION_NETWORK = "0.14.1";
+	const MINECRAFT_VERSION_NETWORK = "0.14.1.0";
 
 	/*
 	 * Startup code. Do not look at it, it may harm you.
